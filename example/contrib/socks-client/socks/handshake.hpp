384ea3458f395be3866bee1a8e97c36f3c3cf6a5
youcai <omegacoleman@gmail.com>
2020-04-21 22:34:55 +0800

    Use string_view.

--- example/contrib/socks-client/socks/handshake.hpp
+++ example/contrib/socks-client/socks/handshake.hpp
@@ -26,9 +26,9 @@ template<
 BOOST_BEAST_ASYNC_RESULT1(Handler)
 async_handshake_v4(
     AsyncStream& stream,
-    const std::string& hostname,
+    string_view hostname,
     unsigned short port,
-    std::string const& username,
+    string_view username,
     Handler&& handler);
 
 /** Perform the SOCKS v5 handshake in the client role.
@@ -39,10 +39,10 @@ template<
 BOOST_BEAST_ASYNC_RESULT1(Handler)
 async_handshake_v5(
     AsyncStream& stream,
-    const std::string& hostname,
+    string_view hostname,
     unsigned short port,
-    std::string const& username,
-    std::string const& password,
+    string_view username,
+    string_view password,
     bool use_hostname,
     Handler&& handler);
 
